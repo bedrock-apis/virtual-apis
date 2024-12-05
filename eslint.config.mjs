f51387d9c0666d601832470fe8b7c1bb742f7891
conmaster2112 <samdembiny@gmail.com>
2024-12-05 15:06:03 +0100

    Types Management

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['src/**/*.{js,mjs,cjs,ts}'] },
   jseslint.configs.recommended,
   ...tseslint.configs.strict,
 ]
--- src/codegen.ts
+++ src/codegen.ts
@@ -1,25 +1,24 @@
-import { parse } from 'acorn';
-import { generate } from 'astring';
+import * as ACORN from 'acorn';
 
-// Sample JavaScript code with modern ES syntax
-const code = `
-  const a = 1;
-  const add = (x, y) => x + y;
-  class MyClass {
-    constructor(name) {
-      this.name = name;
-    }
-    greet() {
-      return \`Hello, \${this.name}\`;
-    }
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type MapToPrintableNode<T> = {[K in keyof T]: T[K] extends ACORN.Node?Node<PrintableNode<T[K]>>:T[K] extends ACORN.Node[]?Node<PrintableNode<T[K][number]>>[]:T[K]}
+export type PrintableNode<N extends ACORN.Node> = Omit<N, "loc" | "start" | "end" | "range">;
+
+export abstract class Node<N extends {type: string}>{
+  public readonly type: N["type"];
+  public constructor(type: N["type"]){
+    this.type = type;
   }
-`;
-
-// Parse the code into an AST
-const ast = parse(code, { ecmaVersion: 'latest', sourceType: 'module' });
-
-// Generate code from the AST
-const generatedCode = generate(ast);
-
-// Print the generated code
-console.log(generatedCode);
+  public toCallExpression(){
+    return new CallExpression(this);
+  }
+}
+export class CallExpression extends Node<ACORN.CallExpression> implements PrintableNode<MapToPrintableNode<ACORN.CallExpression>>{
+  public constructor(node: Node<PrintableNode<ACORN.Node>>){
+    super('CallExpression');
+    this.callee = node;
+  }
+  callee: Node<PrintableNode<ACORN.Expression | ACORN.Super>>;
+  arguments: Node<PrintableNode<ACORN.Expression | ACORN.SpreadElement>>[] = [];
+  public optional: boolean = false;
+}
