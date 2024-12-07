3bc740883cc2d27575945b38030dcf2bb6b05e73
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-12-07 23:16:05 +0300

    Fix tests

--- src/api-builder/kernel.test.ts
+++ src/api-builder/kernel.test.ts
@@ -6,16 +6,18 @@ suite('Kernel', () => {
     expectTypeOf(Kernel.Construct('Number'));
   });
   test('Prototype Isolation', () => {
-    const map1 = new (Kernel.Constructor('Map'))();
-    const map2 = Kernel.Construct('Map');
-    const map3 = new Kernel['globalThis::Map']();
-
-    expect(delete (Kernel['Map::constructor']['prototype'] as any)['set']).toBe(true); // Prototype modification emulation
-
     try {
+      const map1 = new (Kernel.Constructor('Map'))();
+      const map2 = Kernel.Construct('Map');
+      const map3 = new Kernel['globalThis::Map']();
+
+      expect(delete (Kernel['Map::constructor']['prototype'] as any)['set']).toBe(true); // Prototype modification emulation
+
       expect(() => map1.set('Test', 'Test1')).toThrow();
       expect(() => map2.set('Test', 'Test2'));
       expect(() => map3.set('Test', 'Test3')).toThrow();
+    } catch (e) {
+      throw e;
     } finally {
       Kernel['Map::constructor']['prototype']['set'] = Kernel['Map::prototype']['set'];
     }
