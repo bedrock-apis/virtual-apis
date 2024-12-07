58a2d5eda5bb190d3b23e2451810b2698dca05d6
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-12-07 13:18:04 +0300

    Remove system dependent test

--- src/api-builder/api-builder.test.ts
+++ src/api-builder/api-builder.test.ts
@@ -56,19 +56,6 @@ describe('Base API', () => {
       expect(e).toMatchInlineSnapshot(
         `[ReferenceError: Native function [Entity::methodA] object bound to prototype does not exist.]`,
       );
-      expect((e as ReferenceError).stack).toMatchInlineSnapshot(`
-        "ReferenceError: Native function [Entity::methodA] object bound to prototype does not exist.
-            at methodA (C:\\fake-api\\src\\api-builder\\api-builder.ts:58:15)
-            at Object.apply (C:\\fake-api\\src\\api-builder\\api-builder.ts:89:16)
-            at C:\\fake-api\\src\\api-builder\\api-builder.test.ts:52:22
-            at file:///C:/fake-api/node_modules/.pnpm/@vitest+runner@2.1.8/node_modules/@vitest/runner/dist/index.js:146:14
-            at file:///C:/fake-api/node_modules/.pnpm/@vitest+runner@2.1.8/node_modules/@vitest/runner/dist/index.js:533:11
-            at runWithTimeout (file:///C:/fake-api/node_modules/.pnpm/@vitest+runner@2.1.8/node_modules/@vitest/runner/dist/index.js:39:7)
-            at runTest (file:///C:/fake-api/node_modules/.pnpm/@vitest+runner@2.1.8/node_modules/@vitest/runner/dist/index.js:1056:17)
-            at processTicksAndRejections (node:internal/process/task_queues:95:5)
-            at runSuite (file:///C:/fake-api/node_modules/.pnpm/@vitest+runner@2.1.8/node_modules/@vitest/runner/dist/index.js:1205:15)
-            at runSuite (file:///C:/fake-api/node_modules/.pnpm/@vitest+runner@2.1.8/node_modules/@vitest/runner/dist/index.js:1205:15)"
-      `);
     }
   });
 });
