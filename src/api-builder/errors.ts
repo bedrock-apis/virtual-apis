d89eeaba5078e964994407d496245c31b61fcb5b
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-12-07 13:58:46 +0300

    Add errors

--- src/api-builder/errors.ts
+++ src/api-builder/errors.ts
@@ -2,37 +2,52 @@ import { Kernel } from './kernel';
 
 export type Range = { min: number; max: number };
 export type NativeKind = 'function' | 'getter' | 'setter' | 'constructor';
+export type NativeActionKind = 'call' | 'get' | 'set';
+
+const TypeError = Kernel.Constructor('TypeError');
+const ReferenceError = Kernel.Constructor('ReferenceError');
+const Error = Kernel.Constructor('Error');
 
 export const Errors = {
   NewExpected() {
-    return Kernel.Construct('TypeError', true, 'must be called with new');
+    return new TypeError('must be called with new');
   },
 
   NoConstructor(id: string) {
-    return Kernel.Construct('ReferenceError', true, `No constructor for native class '${id}'.`);
+    return new ReferenceError(`No constructor for native class '${id}'.`);
   },
 
   IncorrectNumberOfArguments(t: Range, length: number) {
-    return Kernel.Construct(
-      'TypeError',
-      true,
+    return new TypeError(
       `Incorrect number of arguments to function. Expected ${t.min === t.max ? t.min : `${t.min}-${t.max}`}, received ${length}`,
     );
   },
 
   BoundToPrototype(kind: NativeKind, id: string) {
-    return Kernel.Construct('ReferenceError', true, `Native ${kind} [${id}] object bound to prototype does not exist.`);
+    return new ReferenceError(`Native ${kind} [${id}] object bound to prototype does not exist.`);
   },
 
   NoPrivilege(kind: NativeKind, id: string) {
-    return Kernel.Construct('ReferenceError', true, `Native ${kind} [${id}] does not have required privileges.`);
+    return new ReferenceError(`Native ${kind} [${id}] does not have required privileges.`);
   },
 
   InvalidAmount(min = 0, max = 256) {
-    return Kernel.Construct('Error', true, `Invalid amount. Amount must be greater than ${min} and less than ${max}`);
+    return new Error(`Invalid amount. Amount must be greater than ${min} and less than ${max}`);
   },
 
   InvalidTimeOfDay(min = 0, max = 23999) {
-    return Kernel.Construct('Error', true, `timeOfDay must be between ${min} and ${max} (inclusive)`);
+    return new Error(`timeOfDay must be between ${min} and ${max} (inclusive)`);
+  },
+
+  ItemTypeDoesNotExist(itemType: string) {
+    return new TypeError(`ItemType '${itemType}' does not exists`);
+  },
+
+  NativeOptionalTypeConversationFailed() {
+    return new TypeError('Native optional type conversion failed');
+  },
+
+  FailedTo(action: NativeActionKind, kind: NativeKind, name: string) {
+    return new Error(`Failed to ${action} ${kind} '${name}'`);
   },
 };
