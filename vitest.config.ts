a31bbd665351a0e51516a4522f3c0c9497d9022a
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-12-07 21:47:02 +0300

    Add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,19 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {},
+  test: {
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['html'],
+      include: ['./scripts/src/**/*.ts'],
+      exclude: [
+        'scripts/src/lib/assets/**',
+        'scripts/src/lib/bds/**',
+        'scripts/src/test/**',
+        '**/node_modules/**',
+        '**/*.test.ts',
+        '**/*.spec.ts',
+      ],
+    },
+  },
 });
