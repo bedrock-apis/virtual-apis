46b6a0bc18494c14fddc69fa66fc1ffe8d69c13b
conmaster2112 <64863757+conmaster2112@users.noreply.github.com>
2025-05-25 13:28:48 +0200

    To Do Tracker

--- tools/build/repo/todo.ts
+++ tools/build/repo/todo.ts
@@ -3,25 +3,43 @@ import { readFile } from 'node:fs/promises';
 import { parseSync } from 'oxc-parser';
 import { getFilesRecursive } from '../../utils';
 
+const green = chalk.rgb(103, 175, 82);
 const matchPattern = /\.ts/;
 const excludePattern = /node_modules|git/;
 const todoPattern = /^ *[A-Z]+:/;
-const tasks: Promise<string[]>[] = [];
 for await (const file of getFilesRecursive('.')) {
    if (excludePattern.test(file)) continue;
-   if (matchPattern.test(file)) tasks.push(fileCommentAnchorsExtractor(file));
+   if (matchPattern.test(file)) await fileCommentAnchorsExtractor(file);
 }
-for (const comments of await Promise.all(tasks));
 
-// TODO:TAG Updated constructionId
+// TODO: TAG Updated constructionId
 async function fileCommentAnchorsExtractor(fileName: string): Promise<string[]> {
    const text = await readFile(fileName);
    const ast = parseSync(fileName, text.toString(), { lang: 'ts', sourceType: 'module' });
    const list: string[] = [];
-   for (const comment of ast.comments) {
+   const comments = ast.comments.filter(e => todoPattern.test(e.value));
+   let i = 1;
+   for (const comment of comments) {
       if (!todoPattern.test(comment.value)) continue;
+      const isLast = i === comments.length;
       list.push(comment.value);
-      console.log(chalk.rgb(89, 165, 65)('//', comment.value.replace(/^ */, '')));
+      console.log(
+         green(
+            ` ${comments.length === 1 ? ' ' : isLast ? '└' : i === 1 ? '┌' : '├'} //`,
+            comment.value.replace(/^ */, ''),
+         ),
+      );
+      const [l, o] = getLine(text, comment.start);
+      console.log(green(` ${isLast ? ' ' : '│'} `) + chalk.blackBright(` ${'└'} '${fileName}:${l + 1}:${o}'`));
+      i++;
    }
    return list;
+}
+function getLine(buffer: Uint8Array, position: number): [number, number] {
+   let line = 0;
+   let offset = 0;
+   for (let i = 0; i < position && i < buffer.length; i++, offset++) {
+      if (buffer[i] === 0x0a) void (line++, (offset = 0));
+   }
+   return [line, offset];
 }
