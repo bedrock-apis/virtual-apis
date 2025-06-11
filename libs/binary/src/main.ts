cb5c70441ace1c43e4b9d39bf4daa6b3d9f69de1
💀💀 Con's  Temporal Worker Account 💀💀 <64863757+conmaster2112@users.noreply.github.com>
2025-06-11 17:39:47 +0200

    NBT Test

--- libs/binary/src/main.ts
+++ libs/binary/src/main.ts
@@ -1,4 +1,5 @@
-import { StaticDataProvider } from "../ref-bapi-nbt/base";
+import { GeneralNBTFormatReader, GeneralNBTFormatWriter, StaticDataProvider } from "../ref-bapi-nbt/base";
+import { StaticNBT } from "../ref-bapi-nbt/nbt";
 import { ImageModuleFormatV1 } from "./image-formats";
 
 export * from "./binary";
@@ -6,6 +7,19 @@ export * from "./image-formats";
 
 const data = StaticDataProvider.alloc(256);
 ImageModuleFormatV1.writeMetadata(data, {test: 5});
-console.log(data.uint8Array.subarray(0, data.pointer));
+//console.log(data.uint8Array.subarray(0, data.pointer));
 data.pointer = 0;
-console.log(ImageModuleFormatV1.readMetadata(data));
+//console.log(ImageModuleFormatV1.readMetadata(data));
+import { readFileSync } from 'node:fs';
+
+const arrayBuffer = readFileSync("./level.dat");
+const view = new DataView(arrayBuffer.buffer, arrayBuffer.byteOffset, arrayBuffer.byteLength);
+const ttt = new StaticDataProvider(view, 8);
+const format = new StaticNBT(new GeneralNBTFormatReader, new GeneralNBTFormatWriter);
+
+console.time("NBT 1000x");
+for(let i = 0; i < 1_000; i++) {
+    ttt.pointer = 8;
+    format.readProperty(ttt);
+}
+console.timeEnd("NBT 1000x");
--- packages/virtual-apis/src/main.ts
+++ packages/virtual-apis/src/main.ts
@@ -4,4 +4,5 @@ export * from './context';
 export { ClassDefinition } from './context/class-definition';
 export * as APIFactory from './context/factory';
 export * as TypesValidation from './type-validators';
+
 export const CONTEXT = new Context();
