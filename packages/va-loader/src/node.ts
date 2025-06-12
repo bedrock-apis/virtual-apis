1b68429f180100ddaa44fbcf23422d0ad0ccf7a1
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2025-06-12 23:08:56 +0300

    Add cache

--- packages/va-loader/src/node.ts
+++ packages/va-loader/src/node.ts
@@ -1,9 +1,10 @@
-import { createRequire, registerHooks } from 'node:module';
+import Module, { createRequire, registerHooks } from 'node:module';
 import url from 'node:url';
 import { createPackageCode } from './create-package-code';
 import { getModuleVersions } from './get-module-versions';
 
 const modules = getModuleVersions();
+const moduleCache = new Map<string, Module.ResolveFnOutput>();
 const require = createRequire(import.meta.url);
 const virtualApis = url.pathToFileURL(require.resolve('@bedrock-apis/virtual-apis')).href;
 
@@ -12,9 +13,15 @@ registerHooks({
       const version = modules.get(specifier);
       if (!version) return nextResolve(specifier, context);
 
-      return {
-         url: `data:application/javascript;base64,${btoa(createPackageCode(specifier, version, virtualApis))}`,
-         shortCircuit: true,
-      };
+      const id = specifier + ' ' + version;
+      let module = moduleCache.get(id);
+      if (!module) {
+         module = {
+            url: `data:application/javascript;base64,${btoa(createPackageCode(specifier, version, virtualApis))}`,
+            shortCircuit: true,
+         };
+         moduleCache.set(id, module);
+      }
+      return module;
    },
 });
