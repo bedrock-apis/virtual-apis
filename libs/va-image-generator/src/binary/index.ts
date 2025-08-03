09e762dfaf92e7c2ab47213e467a4caf8cbce8a5
💀 ConMaster 💀 <conmaster@noreply.com>
2025-08-03 19:06:06 +0200

    Some unpushed files

--- libs/va-image-generator/src/binary/index.ts
+++ libs/va-image-generator/src/binary/index.ts
@@ -7,19 +7,22 @@ import { MetadataToSerializableTransformer } from './metadata-to-serializable';
 
 const CIS = CurrentBinaryImageSerializer;
 export async function main(metadataProvider: IMetadataProvider): Promise<number> {
+   let startup_time = performance.now();
    const { modules, metadata } = await new MetadataToSerializableTransformer().transform(metadataProvider);
 
    const buffer = DataCursorView.Alloc(2 ** 16 * 3); // 65536 bytes -> 64 kb
    console.log(CIS.version);
    CIS.WriteGeneralHeader(buffer, metadata);
    for (const { metadata, id, data, stats } of modules) {
+      let size = buffer.pointer;
       console.log(`Write Module ${id}`, stats.uniqueTypes);
       CIS.WriteFieldHeader(buffer, metadata);
       CIS.WriteModuleField(buffer, data);
+      console.log(`Module '${id}', size: ${buffer.pointer - size}`);
    }
 
    const fileBuffer = buffer.getBuffer();
    writeFileSync(path.join(MODULES_DIR, 'image.bin'), fileBuffer);
-   console.log('Done. Size: ', Number((fileBuffer.length / 1024).toFixed(2)), 'kb');
+   console.log('✅ Done...\n📦 Size: ->', Number((fileBuffer.length / 1024).toFixed(2)), 'kb',"\n⌚ Time: " + (performance.now() - startup_time));
    return 0;
 }
