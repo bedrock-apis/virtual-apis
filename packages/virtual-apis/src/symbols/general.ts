267bac2b1b4a0a86852c9bad2d726d88b3a48e67
💀 ConMaster 💀 <conmaster@noreply.com>
2025-08-11 13:04:49 +0200

    Interface Symbol & Map type

--- packages/virtual-apis/src/symbols/general.ts
+++ packages/virtual-apis/src/symbols/general.ts
@@ -2,16 +2,17 @@ import type { Context } from '../context/base';
 
 export abstract class CompilableSymbol<T> {
    // Do not expose this property yet, maybe it will be an map of (Context -> ApiValue)
-   private readonly RUNTIME: T | null = null;
+   // Null means this symbol has no runtime representation after compilation, eg. interfaces
+   private readonly RUNTIME: WeakMap<Context, T | null | undefined> = new WeakMap();
    public readonly name!: string;
-   protected abstract compile(context: Context): T;
+   protected abstract compile(context: Context): T | null;
    protected precompileChecks(_: Context): void {}
    public setName(name: string): this {
       (this as Mutable<this>).name = name;
       return this;
    }
-   public getRuntimeValue(context: Context): T {
-      //@ts-expect-error We expect that this throws as runtime property is not assignable
-      return this.RUNTIME === null ? (this.RUNTIME = this.compile(context)) : this.RUNTIME;
+   public getRuntimeValue(context: Context): T | null {
+      if (!this.RUNTIME.has(context)) this.RUNTIME.set(context, this.compile(context));
+      return this.RUNTIME.get(context) ?? null;
    }
 }
