76813931366ba0654342a0cc30f59e3945d72432
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2025-09-20 15:12:46 +0300

    remove unused code

--- packages/va-image-generator/src/main.ts
+++ packages/va-image-generator/src/main.ts
@@ -1,13 +1,1 @@
-// import { getOrGenerateMetadataFilepaths } from '@bedrock-apis/va-bds-dumps/api';
-// import { main } from './index';
-// import { SystemFileMetadataProvider } from './metadata-provider';
-
-// main(new SystemFileMetadataProvider(...(await getOrGenerateMetadataFilepaths()))).then(
-//    r => (process.exitCode = r),
-//    e => {
-//       console.error(e);
-//       process.exit(-1);
-//    },
-// );
-
-export * from './binary-image-loader'
+export * from './binary-image-loader';
